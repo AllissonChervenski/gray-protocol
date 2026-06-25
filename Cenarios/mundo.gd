194ea3c4f3c6967237f6cede4b07a22936b03406
allissonchervenski <allissonchervenski5@gmail.com>
2026-06-25 01:43:11 -0300

    fix: take weapon with 'E'

--- Cenarios/mundo.gd
+++ Cenarios/mundo.gd
@@ -3,7 +3,7 @@ extends Node2D
 # Cenas instanciadas em tempo de execução (UI e itens do mundo)
 const HUD = preload("res://UI/hud.tscn")
 const GAME_OVER = preload("res://UI/game_over.tscn")
-const ITEM_COLETAVEL = preload("res://Itens/item_coletavel.tscn")
+
 
 
 func _ready() -> void:
@@ -12,6 +12,3 @@ func _ready() -> void:
 	add_child(GAME_OVER.instantiate())
 
 	# Coloca a arma no chão, perto de onde o jogador começa (~Vector2(6, -36))
-	var arma_no_chao = ITEM_COLETAVEL.instantiate()
-	arma_no_chao.position = Vector2(60, -36)
-	add_child(arma_no_chao)
